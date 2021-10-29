35e889a39ef279870c9d170fd96ea4cdd3527fd7
sreyash8580 <78336924+sreyash8580@users.noreply.github.com>
2021-10-29 14:40:52 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-
+    document.getElementById('popout').style.display = 'none';
 
 
     fetch("https://restcountries.com/v3.1/all")
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
     
                 // For Capital 
                 var p = document.createElement("p")
-                p.innerHTML = "Capital: " + country.capital[0]
+                p.innerHTML = "Capital: " + country.capital
     
                 // For Region 
                 // console.log(country.region)
@@ -39,8 +39,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
                 population.innerHTML="Population: " + country.population
     
                 //For Button
-                var btn = document.createElement("p")
-                btn.innerHTML= "<button>Click For Weather</button>"
+                var btn = document.createElement("button")
+                btn.innerHTML= "Click For Weather";
+                btn.addEventListener('click', () => {
+                    getpopupdata(country.name.common);
+                })
     
                 ul.appendChild(div)
                 div.appendChild(h5)
@@ -63,40 +66,40 @@ document.addEventListener('DOMContentLoaded', ()=> {
         .catch(function (err) {
             console.log(err)
         })
-    })
+    
 
 
-//         function getpopupdata(q) {
-//             document.getElementById('popout').style.display = 'flex';
-//             document.getElementById('country-list').style.opacity = '0.7';        
-//             const api = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&appid=9fc1269056a6b8aaf47d1d8b2ca31e07";
-//             fetch(api).then(response => response.json()).then((data) => {
-//                 var country_name = data.name;
-//                 var country_temp = data.main['temp'];
-//                 var country_wind = data.wind['speed'];
-//                 var country_humidity = data.main['humidity'];
-//                 //Grab data and insert it into created elements           
-//                 document.getElementById("span_country_name").innerHTML = country_name;
-//                 document.getElementById("span_humidity").innerHTML = country_humidity;
-//                 document.getElementById("span_country_wind").innerHTML = country_wind;
-//                 document.getElementById("span_country_temp").innerHTML = country_temp;
-//             }).catch(err => {
-//                 alert(err);
-//             })
-//         }
-//         document.getElementById('closeBtn').addEventListener('click', () => {
-//             document.getElementById('popout').style.display = 'none';
-//         })
-//         //Call the function that will automatically run renderCountry() also 
-//         fetchData();
+       function getpopupdata(q) {
+           console.log("inside pop");
+             document.getElementById('popout').style.display = 'flex';
+           document.getElementById('list').style.opacity = '0.7';        
+             const api = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&appid=9fc1269056a6b8aaf47d1d8b2ca31e07";
+             fetch(api).then(response => response.json()).then((data) => {
+                 var country_name = data.name;
+                 var country_temp = data.main['temp'];
+                 var country_wind = data.wind['speed'];
+                 var country_humidity = data.main['humidity'];
+               //Grab data and insert it into created elements           
+                 document.getElementById("span_country_name").innerHTML = country_name;
+                 document.getElementById("span_humidity").innerHTML = country_humidity;
+                 document.getElementById("span_country_wind").innerHTML = country_wind;
+                 document.getElementById("span_country_temp").innerHTML = country_temp;
+             }).catch(err => {
+                 alert(err);
+             })
+         }
+         document.getElementById('closeBtn').addEventListener('click', () => {
+             document.getElementById('popout').style.display = 'none';
+         })
+         //Call the function that will automatically run renderCountry() also 
+         
         
-// })
+})
 
 // fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&appid={9fc1269056a6b8aaf47d1d8b2ca31e07}")
 // .then(function (data) {
 //     return data.json()
 // })
 // .catch(function (err) {
-//     console.log(err)
+//     console.log(err).3
 // })
-
